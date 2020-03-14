6f0f2a2b1478a95caa1e63cbddb6751e6a8bfa92
Grigorii Horos <horosgrisa@gmail.com>
2020-03-14 15:42:54 +0200

    Optimize

--- title.plugin.zsh
+++ title.plugin.zsh
@@ -1,8 +1,7 @@
 _show_title(){
-  print -Pn "\033]0;${PROMPT_TITLE}\007"
+  print -Pn "\033]0;${PROMPT_TITLE:-"$USER@$HOST"}\007"
 }
 
 autoload -Uz add-zsh-hook
 add-zsh-hook precmd _show_title
 _show_title
-
